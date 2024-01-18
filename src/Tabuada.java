bfcd6dd038f68f1c79ced0a4a45ba7b5b6061d3d
Cassio <cassionascimentovieira@outlook.com>
2024-01-18 19:50:08 -0300

    first commit

---
+++ src/Tabuada.java
@@ -0,0 +1,54 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Tabuada {
+    public static void main(String args[]) {
+        String tabuadaDo2 = "2";
+        String tabuadaDo3 = "3";
+        String tabuadaDo4 = "4";
+        String tabuadaDo5 = "5";
+        String tabuadaDo6 = "6";
+        String tabuadaDo7 = "7";
+        String tabuadaDo8 = "8";
+        String tabuadaDo9 = "9";
+        String tabuadaEscolhida = tabuadaDo2 ;
+
+        if ( tabuadaEscolhida == tabuadaDo2) {
+
+            System.out.println(" Resultado da tabuada ");
+            for (int i = 2; i <=20; i = i + 2)
+                System.out.println(  i);
+        }else if ( tabuadaEscolhida == tabuadaDo3){
+            System.out.println(" Resultado da tabuada ");
+            for (int i = 3; i <=20; i = i + 3)
+                System.out.println(  i);
+
+        }else if ( tabuadaEscolhida == tabuadaDo4){
+            System.out.println(" Resultado da tabuada ");
+            for (int i = 4; i <=20; i = i + 4)
+                System.out.println(  i);
+        }else if ( tabuadaEscolhida == tabuadaDo5){
+            System.out.println(" Resultado da tabuada ");
+            for (int i = 5; i <=20; i = i + 5)
+                System.out.println(  i);
+        }else if ( tabuadaEscolhida == tabuadaDo6){
+            System.out.println(" Resultado da tabuada ");
+            for (int i = 6; i <=20; i = i + 6)
+                System.out.println(  i);
+        }else if ( tabuadaEscolhida == tabuadaDo7){
+            System.out.println(" Resultado da tabuada ");
+            for (int i = 7; i <=20; i = i + 7)
+                System.out.println(  i);
+        } else if (tabuadaEscolhida == tabuadaDo8) {
+            System.out.println(" Resulrado da tabuada ");
+            for (int i = 8; i <= 80; i = i+8)
+                System.out.println( i );
+
+        } else if (tabuadaEscolhida == tabuadaDo9) {
+            System.out.println(" Resultadi da tabuada");
+            for (int i =9; i <= 90; i = i+9)
+                System.out.println( i );
+
+        }
+    }
+}
